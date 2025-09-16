195d6fadf8c650a1fa44b8646b05b43fbc7859bc
ytak-sagit <omega.sagittarius.xxx+tech@gmail.com>
2025-09-16 23:03:08 +0900

    feat: チャットサービスのロジックを追加

---
+++ src/chat-service.ts
@@ -0,0 +1,10 @@
+import { ServiceImpl } from "@bufbuild/connect";
+import { ChatService } from "./gen/chat/v1/chat_connect";
+
+export const chatServiceImpl: ServiceImpl<typeof ChatService> = {
+  say: (req, ctx) => {
+    return {
+      sentence: `You said ${req.sentence}`,
+    };
+  },
+};
